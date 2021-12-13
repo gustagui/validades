f842d496d589afb82495306c90e3d5bea6c3007c
Gustavo <gustagui.dev@gmail.com>
2021-12-13 07:35:02 -0300

    limit

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -10,9 +10,13 @@ const UserController = {
     // },
 
     index: async (req, res) => {
-        let users = await Usuario.findAll();
-    
-        return res.render('usuarios', {users})
+        let {page = 1} = req.query
+        let {count:total, rows:users} = await Usuario.findAndCountAll({
+          limit: 5,
+          offset: (page - 1) * 5
+        });
+        let totalPagina = Math.round(total/5)    
+        return res.render('usuarios', {users, totalPagina})
       },
       findById: async (req, res) => {
         let { id } = req.params;
@@ -25,15 +29,18 @@ const UserController = {
 
         return res.render('viewUsuario', {user})
       },
-      search: async(req, res) => {
-        let {key} = req.query
+      search: async (req, res) => {
+        let {key} = req.query;
 
         let users = await Usuario.findAll({
           where:{
             name:{
               [Op.like]: `%${key}%`
             }
-          }
+          },
+          order:[
+            ['id', 'ASC']
+          ]
         });
     
         return res.render('usuarios', {users})
