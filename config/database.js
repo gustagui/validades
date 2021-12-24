2b851243bf64fa323d6159a90d20768fa69618ce
Gustavo <gustagui.dev@gmail.com>
2021-12-23 23:02:28 -0300

    db set

--- config/database.js
+++ config/database.js
@@ -17,7 +17,7 @@ module.exports = {
       username: "root",
       password: null,
       database: "validades",
-      host: "127.0.0.1",
+      host: "mysql://be1d456ac82cd3:ad404816@us-cdbr-east-05.cleardb.net/heroku_506ea39d50d24bd?reconnect=true",
       dialect: "mysql"
     }
   }
